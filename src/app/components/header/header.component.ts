53d70b3142237bbeabc10cae72ade6bde0e7b13e
Luiz Alexandre Silva <luiz.sarmento@fpf.br>
2024-01-29 19:10:46 -0400

    bootstrap

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import {NgbCollapseModule} from "@ng-bootstrap/ng-bootstrap";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgbCollapseModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuCollapsed = true;
 
 }
